5a718ce407c5b9fd55c9780478622858118ab717
CJKbarts <bartelsjacobcorn@gmail.com>
2024-12-24 05:36:09 -0500

    Add one-to-many association between User and Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: { in: 5..32 }
-  validates :body, presence: true, length: { in: 10..100 }
+  validates :body, presence: true, length: { in: 10..100 }\
+
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: { in: 3..12 }
+
+  has_many :posts
 end
